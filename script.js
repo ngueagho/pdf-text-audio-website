f689256056a427beaf981588e85fa686831fe501
ngueagho <ngueaghotiodongrobertolandry@gmail.com>
2024-07-16 14:01:54 +0100

    16-07-024

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function downloadFile() {
     window.location.href = './damso_damso-on-n-est-jamais-mieux-compris-que-par-soi-meme.mp3';
 }
+
+
+
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var navbar = document.getElementById('navbar');
+    if (navbar.style.display === 'flex') {
+        navbar.style.display = 'none';
+    } else {
+        navbar.style.display = 'flex';
+    }
+});
