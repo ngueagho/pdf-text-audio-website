cde6e8d14468b988ee296fc6a4cae9ef7dc939ec
ngueagho <ngueaghotiodongrobertolandry@gmail.com>
2024-07-23 19:07:32 +0100

    23-07-024

--- text-to-speach-v2/script.js
+++ text-to-speach-v2/script.js
@@ -1,12 +1,35 @@
+//Text To Speech Variables
 const textarea = document.querySelector("textarea"),
 voiceList = document.querySelector("select"),
-speechBtn = document.querySelector("button");
+speechBtn = document.getElementById("convert_speech"),
+clearBtn = document.getElementById("clearBtn");
 
 let synth = speechSynthesis,
-isSpeaking = true;
+isSpeaking = true,
+utterance;
 
 voices();
 
+textarea.addEventListener("input", function () {
+    if (textarea.value.length > 0) {
+      // If the textarea has content, add the "show" class to display the button
+      clearBtn.classList.add("show");
+      clearBtn.classList.remove("hide");
+    } else {
+      // If the textarea is empty, add the "hide" class to hide the button
+      clearBtn.classList.add("hide");
+      clearBtn.classList.remove("show");
+    }
+  });
+
+// Clear Button Function
+clearBtn.addEventListener("click", e =>{
+    textarea.value = "";
+    clearBtn.classList.add("hide");
+    clearBtn.classList.remove("show");
+});
+
+// Select Voice Function
 function voices(){
     for(let voice of synth.getVoices()){
         let selected = voice.name === "Google US English" ? "selected" : "";
@@ -17,8 +40,9 @@ function voices(){
 
 synth.addEventListener("voiceschanged", voices);
 
+// Text To Speech Function
 function textToSpeech(text){
-    let utterance = new SpeechSynthesisUtterance(text);
+    utterance = new SpeechSynthesisUtterance(text);
     for(let voice of synth.getVoices()){
         if(voice.name === voiceList.value){
             utterance.voice = voice;
@@ -27,13 +51,14 @@ function textToSpeech(text){
     synth.speak(utterance);
 }
 
+// Speech Button Function
 speechBtn.addEventListener("click", e =>{
     e.preventDefault();
     if(textarea.value !== ""){
         if(!synth.speaking){
             textToSpeech(textarea.value);
         }
-        if(textarea.value.length > 80){
+        if(textarea.value.length > 20){
             setInterval(()=>{
                 if(!synth.speaking && !isSpeaking){
                     isSpeaking = true;
@@ -44,11 +69,11 @@ speechBtn.addEventListener("click", e =>{
             if(isSpeaking){
                 synth.resume();
                 isSpeaking = false;
-                speechBtn.innerText = "Pause Speech";
+                speechBtn.innerText = "Pause";
             }else{
                 synth.pause();
                 isSpeaking = true;
-                speechBtn.innerText = "Resume Speech";
+                speechBtn.innerText = "Resume";
             }
         }else{
             speechBtn.innerText = "Convert To Speech";
