14cf3a32e5249c0b52ae44260aaf4fd82703c7c7
ngueagho <ngueaghotiodongrobertolandry@gmail.com>
2024-07-23 18:51:54 +0100

    23-07-024

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-function downloadFile() {
-    window.location.href = './damso_damso-on-n-est-jamais-mieux-compris-que-par-soi-meme.mp3';
-}
+
 
 
 
@@ -13,3 +11,36 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
         navbar.style.display = 'flex';
     }
 });
+
+document.getElementById('upload-input').addEventListener('change', function(event) {
+    var fileName = event.target.files[0].name;
+    document.getElementById('title').innerText = fileName;
+
+    // Show the modal
+    var modal = document.getElementById('languageModal');
+    modal.style.display = 'block';
+});
+
+document.getElementById('closeModal').addEventListener('click', function() {
+    var modal = document.getElementById('languageModal');
+    modal.style.display = 'none';
+});
+
+// Close the modal if the user clicks outside of it
+window.onclick = function(event) {
+    var modal = document.getElementById('languageModal');
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+};
+
+function downloadFile() {
+    window.location.href = './damso_damso-on-n-est-jamais-mieux-compris-que-par-soi-meme.mp3';
+}
+
+
+
+
+function sidebar0() {
+    document.getElementById("nav-bar1").style.display = 'none'
+}
